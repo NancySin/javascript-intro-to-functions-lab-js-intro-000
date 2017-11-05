d919a252dcd2dea209c3c022331d4d379e016d48
nancy sin <nsin@sbcglobal.net>
2017-11-05 18:10:25 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,22 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function (hello) {
+  return hello.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+function logShout(string) {
+  console.log string.toUpperCase();
+}
+
+function shout(string) {
+  return string.toUpperCase();
+}
+ function hello(string) {
+   return string.toUpperCase ()
+ }
